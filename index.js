23bb96403738ee7388fc2857740fd5b958d97c94
ibbbrrru <106899988+ibbbrrru@users.noreply.github.com>
2022-08-14 12:41:29 +0530

    CRM

---
+++ index.js
@@ -0,0 +1,42 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const balanceToGive = document.querySelector("#balance-to-give");
+const noOfNotes = document.querySelectorAll(".no-of-notes"); // we can't access multiple elements in id. so, here through this class we can access that table in the form of array
+const availableNotes = [2000,500,100,50,20,10,5,2,1];
+
+//checkButton.addEventListener("click", ()=> console.log("clicked"));
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    //message.style.display="none";
+    hideMessage();
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            balanceToGive.innerText = "Balance to give: " + amountToBeReturned + " ₹";
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("Cash provided should be atleast equal to bill amount");
+        }
+    } else {
+        showMessage("Invalid bill amount");
+    }
+
+});
+
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
+
+function hideMessage(msg) {
+    message.style.display = "none";
+}
